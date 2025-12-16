1f46c9827d14c8267ccd2ea8b591515b22858021
pb-06 <kkandomail@gmail.com>
2025-12-16 13:24:20 +0100

    Manage HanoiApplication

--- src/main/java/com/example/hanoi/HanoiApplication.java
+++ src/main/java/com/example/hanoi/HanoiApplication.java
@@ -11,8 +11,8 @@ public class HanoiApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HanoiApplication.class.getResource("hanoi-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Hanoi");
         stage.setScene(scene);
         stage.show();
     }
